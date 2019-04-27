328b78b9255e9b227882fca3cb5bce8cc9366d43
MartinKamenov <martinkamenov10@gmail.com>
2019-04-27 19:54:35 +0300

    Fix connector locally

--- src/database/connector.js
+++ src/database/connector.js
@@ -1,8 +1,9 @@
     
 const Database = require('./Database');
-const credentialManager = require('../../../credentials/credentialManager');
-const username = process.env.dbUser || credentialManager.dbUser;
-const password = process.env.dbPassword || credentialManager.dbPassword;
+const username = process.env.dbUser || 
+    require('../../../credentials/credentialManager').dbUser;
+const password = process.env.dbPassword || 
+    require('../../../credentials/credentialManager').dbPassword;
 
 const connectionstring = `mongodb://${username}:${password}@ds147926.mlab.com:47926/chatdb`;
 const database = new Database(connectionstring);
