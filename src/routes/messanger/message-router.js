7087586cd8dbb7545fb8f65bf178f35b3aa3857a
MartinKamenov <martinkamenov10@gmail.com>
2019-04-27 21:16:15 +0300

    Connect db to messenger controller

--- src/routes/messanger/message-router.js
+++ src/routes/messanger/message-router.js
@@ -1,13 +1,17 @@
 const enableWs = require('express-ws');
 const controller = require('./message-controller');
 
+
 const attach = (app, messengerRepository) => {
     enableWs(app);
     app.ws('/messenger/:id', async (ws, req) => {
         const id = req.params.id;
         controller.addConnection(id, ws);
+        const messenger = await controller.getMessenger(id, messengerRepository);
         ws.on('message', async (msg) => {
             await controller.addMessage(id, 'Pesho', msg);
+            controller
+                .addMessageToDatabase(id, messenger, messengerRepository, { username: 'Pesho', message: msg });
         });
     
         ws.on('close', () => {
