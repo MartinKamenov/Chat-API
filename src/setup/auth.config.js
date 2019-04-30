4c8652cdc91ec16b1ce7bc1a991abf355f18998b
MartinKamenov <martinkamenov10@gmail.com>
2019-04-30 15:04:49 +0300

    Use id for deserialization of user. Add validation messages

--- src/setup/auth.config.js
+++ src/setup/auth.config.js
@@ -3,6 +3,17 @@ const passport = require('passport');
 const { Strategy } = require('passport-local');
 const User = require('../models/User');
 const uuid = require('uuid');
+const NO_USERS_FOUND_MESSAGE = 'No user with such id was found.';
+const COULD_NOT_CONNECT_MESSAGE = 'Could not connect to database.';
+const USERNAME_IS_TAKEN_MESSAGE = 'Username is taken';
+const MORE_THAN_ONE_USER_FOUND_MESSAGE = 'There is more than one user with this id';
+const ALL_FIELDS_ARE_REQUIRED_MESSAGE = 'All fields are required';
+const COULD_NOT_REGITRATE_USER_MESSAGE = 'User could not be registered';
+const PASSWORDS_NOT_SAME_MESSAGE = 'Passwords are not the same';
+const WRONG_USERNAME_OR_PASSWORD_MESSAGE = 'Wrong username or password';
+const PASSWORD_VALIDATION_MESSAGE = 'Password must be at 4 characters long';
+const USERNAME_VALIDATION_MESSAGE = 'Username should consists only of letters and numbers';
+const EMAIL_VALIDATION_MESSAGE = 'E-mail is not valid';
 
 function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -29,11 +40,11 @@ const configAuth = (app, userRepository, registeredUser) => {
                     .then((users) => {
                         if (users.length < 1) {
                             return done(null, false, {
-                                message: 'Wrong username or password',
+                                message: WRONG_USERNAME_OR_PASSWORD_MESSAGE
                             });
                         } else if (users.length > 1) {
                             return done(null, false, {
-                                message: 'Problem with duplicate user',
+                                message: MORE_THAN_ONE_USER_FOUND_MESSAGE
                             });
                         }
                         return done(null, users[0]);
@@ -48,35 +59,35 @@ const configAuth = (app, userRepository, registeredUser) => {
             const repeatpassword = req.body.password_confirm;
             if (!password || !username || !email || !repeatpassword) {
                 return done(null, false, {
-                    message: 'All fields are required!',
+                    message: ALL_FIELDS_ARE_REQUIRED_MESSAGE,
                 });
             }
 
             if (password !== repeatpassword) {
                 return done(null, false, {
-                    message: 'Passwords are not the same!',
+                    message: PASSWORDS_NOT_SAME_MESSAGE,
                 });
             }
             if (password.length < 4) {
                 return done(null, false, {
-                    message: 'Password must be at 4 characters long!',
+                    message: PASSWORD_VALIDATION_MESSAGE,
                 });
             }
             if (!validateEmail(email)) {
                 return done(null, false, {
-                    message: 'The e-mail is not valid',
+                    message: EMAIL_VALIDATION_MESSAGE,
                 });
             }
             if (!validateUsername(username)) {
                 return done(null, false, {
-                    message: 'Username should be alphanumerical',
+                    message: USERNAME_VALIDATION_MESSAGE,
                 });
             }
             return userRepository.findUserByUsername(username)
                 .then((users) => {
                     if (users.length > 0) {
                         return done(null, false, {
-                            message: 'Username is taken',
+                            message: USERNAME_IS_TAKEN_MESSAGE,
                         });
                     }
                     const newUser = new User(uuid.v1, email, username, password);
@@ -85,12 +96,12 @@ const configAuth = (app, userRepository, registeredUser) => {
                             // const message = 'Successfully Registered';
                             done(null, newUser);
                         }).catch(() => {
-                            const message = 'Some problem with registering';
+                            const message = COULD_NOT_REGITRATE_USER_MESSAGE;
                             done(message);
                         });
                 })
                 .catch((err) => {
-                    const message = 'Some problem with users database';
+                    const message = COULD_NOT_CONNECT_MESSAGE;
                     done(message);
                 });
         }
@@ -100,16 +111,16 @@ const configAuth = (app, userRepository, registeredUser) => {
     app.use(passport.session());
 
     passport.serializeUser((user, done) => {
-        done(null, user._id);
+        done(null, user.id);
     });
 
     passport.deserializeUser((id, done) => {
         userRepository.findUserByParams({ id })
             .then((users) => {
                 if (users.length < 1) {
-                    return done('No such user found.----');
+                    return done(NO_USERS_FOUND_MESSAGE);
                 } else if (users.length > 1) {
-                    return done('There is more than one user with this id');
+                    return done(MORE_THAN_ONE_USER_FOUND_MESSAGE);
                 }
                 return done(null, users[0]);
             })
