5de95840cf4ff73004cea9da10793755f04b5d36
MartinKamenov <martinkamenov10@gmail.com>
2019-05-18 18:27:16 +0300

    Assert element paging is working correctly

--- test/elementPagingTests.js
+++ test/elementPagingTests.js
@@ -4,7 +4,7 @@ const defaultPageSize = 10;
 const assert = require('chai').assert;
 for(let i = 0; i < 40; i++) {
     elements.push({
-        id: i,
+        id: '' + i,
         text: 'Text ' + i
     });
 }
@@ -15,5 +15,19 @@ describe('Element paging', function () {
         const pagedElements = elementPaging.getCollectionPage(elements, '', defaultPageSize);
         // Assert
         assert.deepEqual(elements.slice(0, defaultPageSize), pagedElements);
+    });
+
+    it('should return correct page correctly when last element id is passed', function () {
+        // Arrange & Act
+        const pagedElements = elementPaging.getCollectionPage(elements, '9', defaultPageSize);
+        // Assert
+        assert.deepEqual(elements.slice(defaultPageSize, defaultPageSize + defaultPageSize), pagedElements);
+    });
+
+    it('should return correct page from last elements correctly when id is passed', function () {
+        // Arrange & Act
+        const pagedElements = elementPaging.getCollectionPage(elements, '35', defaultPageSize);
+        // Assert
+        assert.deepEqual(elements.slice(35, 40), pagedElements);
     });
 });
